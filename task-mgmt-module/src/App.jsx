d9e33f1a315a896eba271ae6c934b69ce50c8323
rickvibhadhini <rickvi03@gmail.com>
2025-03-04 11:27:17 +0530

    start of frontend work

---
+++ task-mgmt-module/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div className="bg-zinc-900">
+      <p className="text-white">hiii</p>
+      <p className="text-white">hello</p>
+    </div>
+    </>
+  )
+}
+
+export default App
