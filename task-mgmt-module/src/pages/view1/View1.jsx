8b8b00bd890d79aaa492a36147ce7a771fc992cc
rickvibhadhini <rickvi03@gmail.com>
2025-03-17 17:55:01 +0530

    change made in view1

--- task-mgmt-module/src/pages/view1/View1.jsx
+++ task-mgmt-module/src/pages/view1/View1.jsx
@@ -8,7 +8,7 @@ const View1 = () => {
       <p className="text-lg text-gray-700 mt-4">This is a sample page for View 1.</p>
 
       <Link to="/" className="mt-6 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition">
-        Go Back to Home
+        Go Back to Home hi
       </Link>
     </div>
   );
