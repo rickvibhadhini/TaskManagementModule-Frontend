f5ab603fe21748cdae920a7754bffedf6ac288b8
Kousthubha GK <kousthubhagk@gmail.com>
2025-03-19 16:26:23 +0530

    Implemented features: search button functionality, changed legend units, converted time display into hrs, mins, secs, added colors to statuses

--- task-mgmt-module/src/pages/view2/components/tables/PendingTasksTable.jsx
+++ task-mgmt-module/src/pages/view2/components/tables/PendingTasksTable.jsx
@@ -1,8 +1,16 @@
 import React, { useState } from 'react';
-import { Card, Table, Badge, Tooltip, Select } from 'antd';
+import { Card, Table, Badge, Tooltip, Select, Tag } from 'antd';
 import { InfoCircleOutlined } from '@ant-design/icons';
 
 const { Option } = Select;
+
+const statusColors = {
+  ALL: 'default',
+  NEW: 'blue',
+  TODO: 'orange',
+  IN_PROGRESS: 'green',
+  FAILED: 'red'
+};
 
 const PendingTasksTable = ({ tasks, columns, info }) => {
   const [statusFilter, setStatusFilter] = useState('ALL');
@@ -14,6 +22,19 @@ const PendingTasksTable = ({ tasks, columns, info }) => {
   const filteredTasks = statusFilter === 'ALL' 
     ? tasks 
     : tasks.filter(task => task.status === statusFilter);
+
+    const updatedColumns = columns.map((col) => 
+      col.dataIndex === 'status'
+        ? { 
+            ...col, 
+            render: (status) => (
+              <Tag color={statusColors[status] || 'default'}>
+                {status}
+              </Tag>
+            ),
+          }
+        : col
+    );
 
   return (
     <Card
@@ -32,20 +53,26 @@ const PendingTasksTable = ({ tasks, columns, info }) => {
             style={{ width: 140 }} 
             onChange={handleFilterChange}
           >
-            <Option value="ALL">All Statuses</Option>
+            {/* <Option value="ALL">All Statuses</Option>
             <Option value="NEW">NEW</Option>
             <Option value="TODO">TODO</Option>
             <Option value="IN_PROGRESS">IN_PROGRESS</Option>
-            <Option value="FAILED">FAILED</Option>
+            <Option value="FAILED">FAILED</Option> */}
+            {Object.keys(statusColors).map((status) => (
+              <Option key={status} value={status}>
+                <Tag color={statusColors[status]}>{status}</Tag>
+              </Option>
+            ))}
           </Select>
           <Badge count={filteredTasks.length} style={{ backgroundColor: '#faad14' }} />
         </div>
       }
     >
       <Table
-        columns={columns}
+        columns={updatedColumns}
         dataSource={filteredTasks}
         pagination={{ pageSize: 5 }}
+        rowKey="id"
       />
     </Card>
   );
