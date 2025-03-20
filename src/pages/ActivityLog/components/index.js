28dfe82f0eeccec541b5882f0f5b7f38e61c3dde
Aditya Sharma <adityasharma121415@gmail.com>
2025-03-20 08:16:28 +0530

    tried adding index.js

---
+++ src/pages/ActivityLog/components/index.js
@@ -0,0 +1,35 @@
+// // components/index.js (update existing file)
+// // Export all components from subdirectories
+// export * from './filters';
+// export * from './funnels';
+// export * from './graph';
+// export * from './Layout';
+
+// // components/filters/index.js (create new)
+// export { default as FilterPanel } from './FilterPanel';
+
+// // components/funnels/index.js (create new)
+// export { default as FunnelCard } from './FunnelCard';
+// export { default as FunnelView } from './FunnelView';
+// export { default as StatusTimeline } from './StatusTimeline';
+// export { default as TaskGroup } from './TaskGroup';
+// export { default as Tooltip } from './Tooltip';
+
+// // components/graph/index.js (create new)
+// export { default as FunnelSummary } from './FunnelSummary';
+// export { default as GanttChart } from './GanttChart';
+// export { default as TaskSummary } from './TaskSummary';
+// export { default as TaskTimeline } from './TaskTimeline';
+// export { default as TaskTooltip } from './TaskTooltip';
+
+// // components/Layout/index.js (create new)
+// export { default as Header } from './Header';
+// export { default as TabNavigation } from './TabNavigation';
+
+// // services/index.js (create new)
+// export { default as ApplicationListApi } from './ApplicationListApi';
+// export { default as Ganttapi } from './Ganttapi';
+
+// // utils/index.js (create new)
+// export { default as ActivityLog } from './ActivityLog';
+// export { default as Dashboard } from './Dashboard';
